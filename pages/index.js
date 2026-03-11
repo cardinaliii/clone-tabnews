59693679d574108ed0690f1d8682471a239ea786
cardinaliii <pedrocardinalidev@gmail.com>
2026-03-11 16:46:58 +0000

    fixing mistakes

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,21 @@
-js
 function Home() {
-   return <h1>Te amo mãe! ❤️</h1> 
+  return (
+    <>
+      <h1>Te amo mãe! ❤️</h1> 
+      
+      <iframe 
+        data-testid="embed-iframe" 
+        style={{ borderRadius: '12px' }} 
+        src="https://open.spotify.com/embed/track/4yBiZFLXn0n9AUJ5sWO88k?utm_source=generator&theme=0" 
+        width="100%" 
+        height="352" 
+        frameBorder="0" 
+        allowFullScreen="" 
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+        loading="lazy"
+      />
+    </>
+  );
 }
 
 export default Home;
-
-<body>
-    <script src="https://open.spotify.com/embed/iframe-api/v1" async></script> 
-
-    <div id="embed-iframe">
-        window.onSpotifyIframeApiReady = (IFrameAPI) => 
-};
-
-
-<iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/4yBiZFLXn0n9AUJ5sWO88k?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-    </div>
-
-     </body>
-
-     
