d8f8585ceb1cbe44c68078c70ca77caa00c3aef3
cardinaliii <pedrocardinalidev@gmail.com>
2026-03-12 20:58:14 +0000

    adding scripts

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto pra evoluir como dev, implementando o https://www.tabnews.com.br para o https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,20 @@
 function Home() {
   return (
     <>
-      <h1>Te amo mãe! ❤️</h1> 
-      
-      <iframe 
-        data-testid="embed-iframe" 
-        style={{ borderRadius: '12px' }} 
-        src="https://open.spotify.com/embed/track/4yBiZFLXn0n9AUJ5sWO88k?utm_source=generator&theme=0" 
-        width="100%" 
-        height="352" 
-        frameBorder="0" 
-        allowFullScreen="" 
-        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+      <h1>Te amo mãe! ❤️</h1>
+
+      <iframe
+        data-testid="embed-iframe"
+        style={{ borderRadius: "12px" }}
+        src="https://open.spotify.com/embed/track/4yBiZFLXn0n9AUJ5sWO88k?utm_source=generator&theme=0"
+        width="100%"
+        height="352"
+        frameBorder="0"
+        allowFullScreen=""
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
         loading="lazy"
       />
     </>
   );
 }
-
 export default Home;
